9b385eb5347c7c1c52b337ab55bd14a823e7948d
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2023-01-29 21:09:26 +0100

    Photo rendering part done

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import Notiflix from 'notiflix';
+import axios from 'axios';
+
+const searchBox = document.querySelector('input');
+const submitButton = document.querySelector('button');
+const gallery = document.querySelector('.gallery');
+const userKey = '33215953-674c55a945dec9bfe68981b61';
+
+submitButton.addEventListener('click', async event => {
+  event.preventDefault();
+  try {
+    const photos = await getPhoto();
+    renderPhotos(photos);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+async function getPhoto() {
+  const searchedTerm = searchBox.value;
+  try {
+    const response = await axios.get(
+      `https://pixabay.com/api/?key=${userKey}&q=${searchedTerm}&image_type=photo&orientation=horizontal&safesearch=true`
+    );
+    console.log(response.data.hits);
+    return response.data.hits;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function renderPhotos(photos) {
+  let markup = photos.map(
+    photo => `<div class="photo-card">
+  <img src="${photo.previewURL}" alt="${photo.tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes: ${photo.likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views: ${photo.views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments: ${photo.comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads: ${photo.downloads}</b>
+    </p>
+  </div>
+</div>`
+  );
+  gallery.innerHTML = markup;
+}
