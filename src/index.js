bad95309fb287843cbed6ef4c5a938db33980c5b
rafalszewczyk890 <rafal.szewczyk890@gmail.com>
2023-01-30 15:21:29 +0100

    Implemented Load More button

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ import axios from 'axios';
 
 const searchBox = document.querySelector('input');
 const submitButton = document.querySelector('button');
+const loadButton = document.querySelector('.load-more');
 const gallery = document.querySelector('.gallery');
 const userKey = '33215953-674c55a945dec9bfe68981b61';
+
+let page = 1;
 
 submitButton.addEventListener('click', async event => {
   event.preventDefault();
@@ -12,6 +15,25 @@ submitButton.addEventListener('click', async event => {
     const photos = await getPhoto();
     if (photos.length > 0) {
       renderPhotos(photos);
+      loadButton.classList.remove('hidden');
+      page += 1;
+    } else {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+loadButton.addEventListener('click', async event => {
+  event.preventDefault();
+  try {
+    const photos = await getPhoto();
+    if (photos.length > 0) {
+      renderPhotos(photos);
+      page += 1;
     } else {
       Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
@@ -26,9 +48,8 @@ async function getPhoto() {
   const searchedTerm = searchBox.value;
   try {
     const response = await axios.get(
-      `https://pixabay.com/api/?key=${userKey}&q=${searchedTerm}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`
+      `https://pixabay.com/api/?key=${userKey}&q=${searchedTerm}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
     );
-    console.log(response.data.hits);
     return response.data.hits;
   } catch (error) {
     console.error(error);
@@ -57,5 +78,5 @@ function renderPhotos(photos) {
 </div>`
     )
     .join('');
-  gallery.innerHTML = markup;
+  gallery.insertAdjacentHTML('beforeend', markup);
 }
